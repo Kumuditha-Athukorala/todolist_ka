068fb370db9ea7b117d051214b7bbc3b6da65014
Kumuditha Athukorala <55691339+Kumuditha-Athukorala@users.noreply.github.com>
2021-01-12 10:30:17 +0000

    Initial Project
    Initial structure of the project added.

---
+++ src/main/java/com/todolist/todolist_ka/TodolistKaApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.todolist_ka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistKaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistKaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/todolist_ka/TodolistKaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.todolist_ka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistKaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
